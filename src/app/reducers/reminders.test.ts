24c22cb03a030219a37082c38c71760a1f10a700
Javier Casas <javcasas@gmail.com>
2019-08-06 18:48:25 -0500

    Tested reducer

--- src/app/reducers/reminders.test.ts
+++ src/app/reducers/reminders.test.ts
@@ -1,7 +1,45 @@
-//import { ReminderState } from './state';
-//import { ReminderAction } from 'app/actions/reminders';
+import { ReminderState } from './state';
+import { ReminderModel, Color } from '../../app/models/ReminderModel';
+import { reminderReducer } from './reminders';
+import { utc } from 'moment'
 
-describe("ASDFSDA", () => {
-  it("BDFGBDF", () => {
+describe("reminderReducer", () => {
+  const reminder : ReminderModel = {
+    id: 0,
+    text: "A reminder",
+    city: "London, UK",
+    date: utc(),
+    color: Color.Red
+  }
+
+  it("action: ADD_REMINDER", () => {
+    expect(
+      reminderReducer(undefined, {type: "ADD_REMINDER", reminder})
+    ).toStrictEqual({
+      nextId: 2,
+      reminders: [
+        {...reminder, id: 1}
+      ]
+    });
+  })
+
+  it("action: UPDATE_REMINDER", () => {
+    const prevState : ReminderState = {
+      nextId: 2,
+      reminders: [{...reminder, id: 1}]
+    }
+    expect(
+      reminderReducer(
+        prevState, 
+        { 
+          type: "UPDATE_REMINDER",
+          updated: {...reminder, id: 1, text: "Updated text"}
+        })
+    ).toStrictEqual({
+      nextId: 2,
+      reminders: [
+        {...reminder, id: 1, text: "Updated text"}
+      ]
+    });
   })
 })
